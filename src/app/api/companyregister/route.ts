bb04aeba422a0b337064f39ed54ce27b0aa1c100
Yash Anand <yash.anand167@gmail.com>
2024-10-12 00:40:29 +0530

    Refactor CompanyRegisterForm component and add company registration functionality

--- src/app/api/companyregister/route.ts
+++ src/app/api/companyregister/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 import bcrypt from "bcryptjs";
 
-
 export async function POST(request: Request) {
   try {
     const client = await clientPromise;
@@ -18,9 +17,18 @@ export async function POST(request: Request) {
     }
 
     const { name, email, password } = payload;
+
+    const existingCompany = await db.collection("companies").findOne({ name, email });
+    if (existingCompany) {
+      return NextResponse.json({
+        error: "A company with this name and email already exists.",
+        status: 409, 
+      });
+    }
+
     const salt = 10;
     const hashedpassword = await bcrypt.hash(password, salt);
-
+    
     const company = {
       name,
       email,
@@ -31,10 +39,10 @@ export async function POST(request: Request) {
     await db.collection("companies").insertOne(company);
 
     return NextResponse.json({
-        message: "Company successfully registered",
-        status: 200,
-
-    })
+      message: "Company successfully registered",
+      company,
+      status: 200,
+    });
   } catch (error) {
     return NextResponse.json({
       error: "Internal Server Error",
