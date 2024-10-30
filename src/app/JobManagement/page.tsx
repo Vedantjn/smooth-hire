13da70a8e28f33a1c59d79bac48dfb637260fed5
Usman Abdullahi Olukayode <olukayodeusman406@gmail.com>
2024-10-30 00:50:03 +0000

    created job management page
    Signed-off-by: Usman Abdullahi Olukayode <olukayodeusman406@gmail.com>

---
+++ src/app/JobManagement/page.tsx
@@ -0,0 +1,11 @@
+// import Image from "next/image";
+import { JobManagementComponent } from "@/components/job-management";
+
+
+export default function Home() {
+  return (
+      <div className="">
+                <JobManagementComponent />
+                    </div>
+                          );
+                        }
